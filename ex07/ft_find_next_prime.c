0a6b8f1ebf8374bf17fe3bc6933ec9ed298bbfe0
Paulo Santana <paulo.santana.r@gmail.com>
2021-04-14 20:19:30 -0300

    fix next prime

--- ex07/ft_find_next_prime.c
+++ ex07/ft_find_next_prime.c
@@ -34,10 +34,8 @@ int	ft_is_prime(int nb)
 
 int	ft_find_next_prime(int nb)
 {
-	if (nb < 1)
+	if (nb < 3)
 		return (2);
-	if (nb == 2)
-		return (3);
 	while (nb < 2147483647)
 	{
 		if (ft_is_prime(nb))
