648e87912b7fa4eabbe6fc74aeeb4d3cc4a9ce05
Paulo Santana <paulo.santana.r@gmail.com>
2021-04-14 20:15:47 -0300

    break fibonacci

--- ex04/ft_fibonacci.c
+++ ex04/ft_fibonacci.c
@@ -10,25 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
-
-int	g_previous_answers[47];
-
 int	ft_fibonacci(int index)
 {
-	int	result;
-
 	if (index < 0)
 		return (-1);
 	if (index == 0)
 		return (0);
 	if (index == 1)
 		return (1);
-	if (index >= 47)
-		return (0);
-	if (g_previous_answers[index] != 0)
-		return (g_previous_answers[index]);
-	result = ft_fibonacci(index - 1) + ft_fibonacci(index - 2);
-	g_previous_answers[index] = result;
-	return (result);
+	return (ft_fibonacci(index -1) + ft_fibonacci(index - 2));
 }
