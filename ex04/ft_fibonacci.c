5ffe69529fdc8e3d1d9b7a6edede5c076d2aa774
Paulo Santana <paulo.santana.r@gmail.com>
2021-04-14 10:38:13 -0300

    fix fibonacci performance

--- ex04/ft_fibonacci.c
+++ ex04/ft_fibonacci.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+
+int	g_previous_answers[47];
+
 int ft_fibonacci(int index)
 {
 	if (index < 0)
@@ -6,5 +10,11 @@ int ft_fibonacci(int index)
 		return (0);
 	if (index == 1)
 		return (1);
-	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+	if (index >= 48)
+		return (0);
+	if (g_previous_answers[index] != 0)
+		return (g_previous_answers[index]);
+	int result = ft_fibonacci(index - 1) + ft_fibonacci(index - 2);
+	g_previous_answers[index] = result;
+	return (result);
 }
